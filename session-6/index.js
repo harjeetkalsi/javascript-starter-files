e414522187c76b0e40596a72714280ab1bfb6c62
harjeet <harjeet.kalsi@solirius.com>
2023-07-11 19:45:22 +0100

    Added new solutions

--- session-6/index.js
+++ session-6/index.js
@@ -3,8 +3,15 @@ const randomNumber = Math.round(Math.random() * 10);
 
 // ****************************************************TASK 1****************************************************
 
+let i = 0; 
+while (i <=10){
+    console.log(i);
+    i +=1;
+}
 
-
+for (let i = 0; i <=10; i+=1){
+    console.log(i);
+}
 
 
 
--- session-7/index.js
+++ session-7/index.js
@@ -1,30 +1,28 @@
 // ****************************************************TASK 1****************************************************
 
+let fruits = ['mango', 'strawberry', 'pear', 'apple'];
 
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits.slice(-1));
+fruits.push("watermelon");
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+fruits.sort();
+console.log(fruits);
 
 // ****************************************************TASK 2****************************************************
 
 
+let names = ["Harjeet", "Tash", "Lotanna", "Steffi", "Fummi", "Victoria","Sundus", "Monaire" , "Tabitha", "Judith"];
 
+console.log(names.length);
+console.log(names.sort());
+console.log(names.sort().reverse());
 
-
-
-
-
-
-
+let numbers = [ 5, 325, 67, 100000, 150];
+console.log(numbers.sort());
 
 // ****************************************************TASK 3****************************************************
 
@@ -40,3 +38,21 @@ const people = [
 function compare(a, b) {
   return a.age - b.age;
 }
+
+for (let index = 0; index < people.length - 1; index++){
+   let person_a = people[index];
+   let person_b = people[index+1];
+
+   let difference = compare(person_a, person_b);
+
+   if (difference > 0){
+    people[index] = person_b;
+    people[index+1] = person_a;
+   };
+  
+};
+
+const names_in_order_age = people.map((person) => person.name);
+
+
+console.log(names_in_order_age);
